dcf0736d2239cd023b482a84ff26e84650a7f3e0
teddy-choe <aackddlr@gmail.com>
2022-07-07 01:06:49 +0900

    [chore] 리뷰 작성 중 취소 Bottom Sheet 컴포저블 추가

---
+++ app/src/main/java/com/mashup/zuzu/ui/review/ReviewWriteScreen.kt
@@ -0,0 +1,111 @@
+package com.mashup.zuzu.ui.review
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.zuzu_android.R
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ReviewWriteScreen() {
+    val bottomSheetScaffoldState = rememberBottomSheetScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
+    BottomSheetScaffold(
+        scaffoldState = bottomSheetScaffoldState,
+        sheetShape = RoundedCornerShape(24.dp, 24.dp),
+        sheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+                    .height(200.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.review_cancel_title),
+                    style = TextStyle(
+                        fontSize = 18.sp
+                    )
+                )
+
+                Text(
+                    text = stringResource(R.string.review_cancel_content),
+                    style = TextStyle(
+                        fontSize = 14.sp
+                    ),
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 48.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(end = 6.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.cancel),
+                            style = TextStyle(
+                                fontSize = 16.sp
+                            )
+                        )
+                    }
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(start = 6.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.confirm),
+                            style = TextStyle(
+                                fontSize = 16.sp
+                            )
+                        )
+                    }
+                }
+            }
+        }, sheetPeekHeight = 0.dp
+    ) {
+        // Main Screen
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Button(onClick = {
+                coroutineScope.launch {
+                    if (bottomSheetScaffoldState.bottomSheetState.isCollapsed) {
+                        bottomSheetScaffoldState.bottomSheetState.expand()
+                    } else {
+                        bottomSheetScaffoldState.bottomSheetState.collapse()
+                    }
+                }
+            }) {
+                Text(text = "test")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ReviewWriteScreenWithBottomSheet() {
+    MaterialTheme {
+        ReviewWriteScreen()
+    }
+}
