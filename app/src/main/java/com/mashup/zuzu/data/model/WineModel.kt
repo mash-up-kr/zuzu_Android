aa708fbf0a4348f6af27bc7689b8566bbb862327
boris <boris0815@naver.com>
2022-07-09 21:12:49 +0900

    [feature] Wine 더미 데이터 생성
    더미데이터 임시로 넣었습니다.

---
+++ app/src/main/java/com/mashup/zuzu/data/model/WineModel.kt
@@ -0,0 +1,109 @@
+package com.mashup.zuzu.data.model
+
+import androidx.compose.runtime.Immutable
+import com.example.zuzu_android.R
+
+/**
+ * @Created by 김현국 2022/07/01
+ * @Time 3:34 오후
+ */
+
+@Immutable
+data class Wine(
+    val id: Long,
+    val name: String,
+    val imageUrl: Int,
+    val price: Long,
+    val alc: Int,
+    val description: List<String>,
+    val favorite: Boolean,
+    val category: String
+)
+
+val wines = listOf(
+    Wine(
+        id = 1L,
+        name = "GoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlue",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 1000,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술", "쓴 술", "단 술"),
+        favorite = true,
+        category = "와인"
+    ),
+    Wine(
+        id = 2L,
+        name = "Jack Daniels",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 40000,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술"),
+        favorite = false,
+        category = "양주"
+    ),
+    Wine(
+        id = 3L,
+        name = "참이슬",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 4500,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술"),
+        favorite = false,
+        category = "소주"
+    ),
+    Wine(
+        id = 4L,
+        name = "로즈와인",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 24000,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술"),
+        favorite = false,
+        category = "와인"
+    ),
+    Wine(
+        id = 5L,
+        name = "진로",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 4000,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술"),
+        favorite = false,
+        category = "소주"
+    ),
+    Wine(
+        id = 6L,
+        name = "Jim Beam",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 24000,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술"),
+        favorite = false,
+        category = "양주"
+    ),
+    Wine(
+        id = 7L,
+        name = "Red Wine",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 3400,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술"),
+        favorite = false,
+        category = "와인"
+    ),
+    Wine(
+        id = 8L,
+        name = "Tintied Wine",
+        imageUrl = R.drawable.img_wine_dummy,
+        price = 40000,
+        alc = 17,
+        listOf("뜨는 술", "맛있는 술"),
+        favorite = false,
+        category = "와인"
+    )
+)
+object WineRepo {
+    fun getWineData(): List<Wine> = wines
+    fun getWineData(wineId: Long) = wines.find { it.id == wineId }
+    fun getWineDataWithCategory(category: String) = wines.filter { it.category == category }
+}
