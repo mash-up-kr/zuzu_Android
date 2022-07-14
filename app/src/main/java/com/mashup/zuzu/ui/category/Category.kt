ef4d1b8c502be31bcd164eb1099bd30e054e989a
Hyunkuk <boris0815@naver.com>
2022-07-14 18:29:00 +0900

    [refactor] 카테고리 스크린 수정
    - dropdown menu제거
    - (with)categoryState 제거 -> 굳이 묶을 필요가 없는것 같음
    - categoryViewModel연결
    - 자잘한 ui 수정

--- app/src/main/java/com/mashup/zuzu/ui/category/Category.kt
+++ app/src/main/java/com/mashup/zuzu/ui/category/Category.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
@@ -45,7 +44,6 @@ import kotlin.math.absoluteValue
  * @Time 5:51 오후
  */
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CategoryScreen(
     modifier: Modifier,
@@ -59,152 +57,101 @@ fun CategoryScreen(
     val index = categoryList.withIndex().filter { it.value.title == category }.map { it.index }
     val categoryState = rememberCategoryState(index[0])
     val wineState = categoryViewModel.wineList.collectAsState().value
-    with(categoryState) {
-        Column(
-            modifier = modifier
+
+    Column(
+        modifier = modifier
+    ) {
+        Icon(
+            modifier = Modifier
+                .padding(top = 30.dp, bottom = 24.dp, start = 24.dp).clickable {
+                    onBackButtonClick()
+                },
+            imageVector = Icons.Outlined.ArrowBack,
+            tint = ProofTheme.color.white,
+            contentDescription = null
+        )
+
+        // 상단 카테고리 선택바
+        CustomScrollableTabRow(
+            tabs = categoryList,
+            selectedTabIndex = categoryState.selectedTabIndex
+        ) { tabIndex ->
+            categoryState.updateSelectedTabIndex(tabIndex)
+            // 여기서 아이템 갱신
+            categoryViewModel.getWineList(categoryList[tabIndex].title)
+        }
+
+        Row(
+            modifier = Modifier.align(Alignment.End).padding(end = 25.dp, top = 15.dp)
         ) {
-
-            Icon(
-                modifier = Modifier
-                    .padding(top = 30.dp, bottom = 24.dp, start = 24.dp).clickable {
-                        onBackButtonClick()
-                    },
-                imageVector = Icons.Outlined.ArrowBack,
-                tint = ProofTheme.color.white,
-                contentDescription = null
-            )
-
-            // 상단 카테고리 선택바
-            CustomScrollableTabRow(
-                tabs = categoryList,
-                selectedTabIndex = selectedTabIndex
-            ) { tabIndex ->
-                updateSelectedTabIndex(tabIndex)
-                // 여기서 아이템 갱신
-                categoryViewModel.getWineList(categoryList[tabIndex].title)
-            }
-
-            Row(
-                modifier = Modifier.fillMaxWidth().padding(start = 24.dp, top = 22.dp, bottom = 31.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                // 정렬 기준 최신 ,오래된순 dropdownmenu
-                ExposedDropdownMenuBox(
-                    expanded = expanded, onExpandedChange = {
-                        updateExpanded()
-                    }
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            modifier = Modifier.wrapContentWidth()
-                                .height(21.dp),
-                            text = sortMenuItemText,
-                            maxLines = 1,
-                            color = ProofTheme.color.white,
-                            fontSize = 13.sp,
-
-                        )
-                        Icon(
-                            imageVector = Icons.Outlined.ExpandMore,
-                            contentDescription = null,
-                            modifier = Modifier.width(16.dp).height(21.dp),
-                            tint = ProofTheme.color.white
-                        )
-                    }
-
-                    ExposedDropdownMenu(
-                        modifier = Modifier.width(120.dp),
-                        expanded = expanded,
-                        onDismissRequest = {
-                            updateExpanded()
+            // viewMode grid or horizontal scroll pager
+            Row() {
+                if (categoryState.viewMode) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_pager),
+                        contentDescription = null,
+                        modifier = Modifier.clickable {
+                            categoryState.changeViewMode()
                         }
-                    ) {
-                        sortMenuItem.forEach { selectedSortMenu ->
-                            DropdownMenuItem(onClick = {
-                                updateSortMenuItemText(selectedSortMenu)
-                                updateExpanded()
-                            }) {
-                                Text(
-                                    text = selectedSortMenu,
-                                    fontSize = 13.sp
-                                )
-                            }
-                        }
-                    }
+                    )
+                    Spacer(modifier = Modifier.width(21.dp).wrapContentHeight())
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_grid_selected),
+                        contentDescription = null,
+                        modifier = Modifier.clickable(enabled = false) {}
+
+                    )
+                } else {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_pager_selected),
+                        contentDescription = null,
+                        modifier = Modifier.clickable(enabled = false) {}
+                    )
+                    Spacer(modifier = Modifier.width(21.dp).wrapContentHeight())
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_grid),
+                        contentDescription = null,
+                        modifier = Modifier.clickable {
+                            categoryState.changeViewMode()
+                        },
+                    )
                 }
-                // viewMode grid or horizontal scroll pager
-                Row(
-                    modifier = Modifier.padding(end = 26.5.dp)
-                ) {
-                    if (viewMode) {
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_pager),
-                            contentDescription = null,
-                            modifier = Modifier.clickable {
-                                changeViewMode()
-                            }
-                        )
-                        Spacer(modifier = Modifier.width(15.dp).wrapContentHeight())
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_grid_selected),
-                            contentDescription = null,
-                            modifier = Modifier.clickable(enabled = false) {}
+            }
+        }
 
-                        )
-                    } else {
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_pager_selected),
-                            contentDescription = null,
-                            modifier = Modifier.clickable(enabled = false) {}
-                        )
-                        Spacer(modifier = Modifier.width(15.dp).wrapContentHeight())
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_grid),
-                            contentDescription = null,
-                            modifier = Modifier.clickable {
-                                changeViewMode()
-                            },
+        // 하단 wine items
+        when (wineState) {
+            is CategoryUiState.Loading -> {
+                // some indicator or loading screen
+            }
+            is CategoryUiState.Success -> {
+                if (categoryState.viewMode)
+                    CategoryWineItems(
+                        wines = wineState.wineList, onWineBoardClick = onWineBoardClick,
+                        modifier = Modifier
+                            .padding(top = 28.dp)
+                            .fillMaxWidth()
+                    )
+                else {
+                    HorizontalPagerWithOffsetTransition(
+                        modifier = Modifier.wrapContentHeight().fillMaxWidth()
+                            .padding(top = 28.dp),
+                        onWineBoardClick = onWineBoardClick,
+                        wines = wineState.wineList
+                    )
+                    Box(
+                        modifier = Modifier.fillMaxHeight().fillMaxWidth().padding(top = 16.dp)
+                    ) {
+                        RoundedButton(
+                            modifier = Modifier.align(Center)
+                                .height(41.dp).width(148.dp),
+                            onButtonClick = { /*TODO*/ }, fontSize = 13.sp, text = "+ 내 술 저장고에 추가"
                         )
                     }
                 }
             }
-
-            // 하단 wine items
-            when (wineState) {
-                is CategoryUiState.Loading -> {
-                    // some indicator or loading screen
-                }
-                is CategoryUiState.Success -> {
-                    if (viewMode)
-                        CategoryWineItems(
-                            wines = wineState.wineList, onWineBoardClick = onWineBoardClick,
-                            modifier = Modifier
-                                .padding(top = 28.dp, start = 24.dp, end = 24.dp)
-                                .fillMaxWidth()
-                        )
-                    else {
-                        HorizontalPagerWithOffsetTransition(
-                            modifier = Modifier.wrapContentHeight().fillMaxWidth(),
-                            onWineBoardClick = onWineBoardClick,
-                            wines = wineState.wineList
-                        )
-                        Box(
-                            modifier = Modifier.fillMaxHeight().fillMaxWidth()
-                        ) {
-                            RoundedButton(
-                                modifier = Modifier.align(Center)
-                                    .height(41.dp).width(148.dp),
-                                onButtonClick = { /*TODO*/ }, fontSize = 13.sp, text = "+ 내 술 저장고에 추가"
-                            )
-                        }
-                    }
-                }
-                is CategoryUiState.Error -> {
-                    // some toast message ?
-                }
+            is CategoryUiState.Error -> {
+                // some toast message ?
             }
         }
     }
@@ -356,18 +303,18 @@ fun CategoryWineItems(
     onWineBoardClick: (Wine) -> Unit
 ) {
     LazyVerticalGrid(
+        modifier = modifier.padding(start = 20.dp, end = 20.dp),
         columns = GridCells.Fixed(2),
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(30.dp)
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+        horizontalArrangement = Arrangement.spacedBy(20.dp),
     ) {
         items(wines) { wine ->
             Box(
                 contentAlignment = Center
             ) {
-                WineBoardCard(
-                    modifier = Modifier
-                        .height(166.dp)
-                        .width(130.dp),
+                WineCardInHome(
+                    modifier = Modifier.fillMaxWidth(),
+                    height = 210.dp,
                     wine = wine, onWineBoardClick = onWineBoardClick
                 )
             }
@@ -375,7 +322,7 @@ fun CategoryWineItems(
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun PreviewCategoryScreen() {
     ProofTheme() {
@@ -383,8 +330,7 @@ fun PreviewCategoryScreen() {
             modifier = Modifier.fillMaxHeight().fillMaxWidth().background(color = ProofTheme.color.black),
             category = "전부",
             onBackButtonClick = {},
-            onWineBoardClick = {},
-            categoryViewModel = CategoryViewModel("전부")
+            onWineBoardClick = {}
         )
     }
 }
