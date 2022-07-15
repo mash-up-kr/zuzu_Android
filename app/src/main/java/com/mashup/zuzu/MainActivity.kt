466abf80f83519a876ea5511156e7cfee2d91115
teddy-choe <aackddlr@gmail.com>
2022-07-15 21:51:23 +0900

    [feature] 리뷰 작성 첫번재 페이지 기능 구현
    - 기능 구조 설계 및 구현
    - 버튼 생성 구현
    - Hilt 주입 관련 의존성 변경

--- app/src/main/java/com/mashup/zuzu/MainActivity.kt
+++ app/src/main/java/com/mashup/zuzu/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.mashup.zuzu.ui.theme.ProofTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/mashup/zuzu/ZuzuApp.kt
+++ app/src/main/java/com/mashup/zuzu/ZuzuApp.kt
@@ -1,6 +1,7 @@
 package com.mashup.zuzu
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -24,76 +25,81 @@ import com.mashup.zuzu.ZuzuAppState.Companion.REVIEW_DETAIL
 import com.mashup.zuzu.ZuzuAppState.Companion.REVIEW_WRITE
 import com.mashup.zuzu.ui.DetailScreen
 import com.mashup.zuzu.ui.home.ZuzuBottomNavigationBar
-import com.mashup.zuzu.ui.review.ReviewWriteScreen
+import com.mashup.zuzu.ui.review.ReviewWriteRoute
 import com.mashup.zuzu.ui.theme.ProofTheme
 
 /**
  * @Created by 김현국 2022/06/30
  * @Time 4:41 오후
  */
+//TODO: Naming 변경 필요함
 @Composable
 fun ZuzuApp() {
     val zuzuAppState = rememberAppState()
 
-    Scaffold(
-        floatingActionButtonPosition = FabPosition.Center,
-        isFloatingActionButtonDocked = true,
-        floatingActionButton = {
-            FloatingActionButton(
-                modifier = Modifier
-                    .width(48.dp)
-                    .height(48.dp),
-                shape = CircleShape,
-                onClick = {
-                    zuzuAppState.navigateToBottomBarRoute(BOTTOM_SCREEN_WORLD_CUP)
-                },
-                backgroundColor = Color.White
-            ) {
-                Image(
+    ProofTheme {
+        Scaffold(
+            floatingActionButtonPosition = FabPosition.Center,
+            isFloatingActionButtonDocked = true,
+            floatingActionButton = {
+                FloatingActionButton(
                     modifier = Modifier
-                        .width(24.dp)
-                        .height(24.dp),
-                    painter = painterResource(id = R.drawable.ic_worldcup),
-                    contentDescription = null
-                )
-            }
-        },
-        bottomBar = {
-            if (zuzuAppState.shouldShowBottomBar) // bottomBarTabs의 BottomScreen의 경로에 있을 때만, BottomNavBar가 보이도록 했습니다.
-                ZuzuBottomNavigationBar(
-                    zuzuAppState.currentRoute,
-                    onBottomTabsClick = { route ->
-                        zuzuAppState.navigateToBottomBarRoute(route)
+                        .width(48.dp)
+                        .height(48.dp),
+                    shape = CircleShape,
+                    onClick = {
+                        zuzuAppState.navigateToBottomBarRoute(BOTTOM_SCREEN_WORLD_CUP)
                     },
-                    bottomNavigationItems = zuzuAppState.bottomBarRoutes
-                )
-        }
-    ) { paddingValues ->
-        NavHost(
-            modifier = Modifier.padding(paddingValues),
-            navController = zuzuAppState.navController,
-            startDestination = REVIEW_DETAIL
-        ) {
-            composable(BOTTOM_SCREEN_NAVIGATION) {
+                    backgroundColor = Color.White
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .width(24.dp)
+                            .height(24.dp),
+                        painter = painterResource(id = R.drawable.ic_worldcup),
+                        contentDescription = null
+                    )
+                }
+            },
+            bottomBar = {
+                if (zuzuAppState.shouldShowBottomBar) // bottomBarTabs의 BottomScreen의 경로에 있을 때만, BottomNavBar가 보이도록 했습니다.
+                    ZuzuBottomNavigationBar(
+                        zuzuAppState.currentRoute,
+                        onBottomTabsClick = { route ->
+                            zuzuAppState.navigateToBottomBarRoute(route)
+                        },
+                        bottomNavigationItems = zuzuAppState.bottomBarRoutes
+                    )
+            },
+            modifier = Modifier.background(ProofTheme.color.black)
+        ) { paddingValues ->
+            NavHost(
+                modifier = Modifier.padding(paddingValues),
+                navController = zuzuAppState.navController,
+                startDestination = REVIEW_DETAIL
+            ) {
+                composable(BOTTOM_SCREEN_NAVIGATION) {
 //                 ZuzuHomeScreen()
-            }
+                }
 
-            composable(BOTTOM_SCREEN_USER) {
-            }
+                composable(BOTTOM_SCREEN_USER) {
+                }
 
-            composable(BOTTOM_SCREEN_WORLD_CUP) {
-            }
+                composable(BOTTOM_SCREEN_WORLD_CUP) {
+                }
 
-            composable(REVIEW_DETAIL) {
-                DetailScreen(
-                    navigateToReviewWrite = { zuzuAppState.navigateReviewWriteScreen() }
-                )
-            }
+                composable(REVIEW_DETAIL) {
+                    DetailScreen(
+                        navigateToReviewWrite = { zuzuAppState.navigateReviewWriteScreen() }
+                    )
+                }
 
-            composable(REVIEW_WRITE) {
-                ReviewWriteScreen()
+                composable(REVIEW_WRITE) {
+                    ReviewWriteRoute()
+                }
             }
         }
+
     }
 }
 
