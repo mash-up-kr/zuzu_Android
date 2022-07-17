15ec0a719c660c6d7442c90b2181d060e40b93f6
teddy-choe <aackddlr@gmail.com>
2022-07-17 16:32:19 +0900

    [chore] timber 의존성 추가 및 초기화 코두 추가

--- app/src/main/java/com/mashup/zuzu/ProofApplication.kt
+++ app/src/main/java/com/mashup/zuzu/ProofApplication.kt
@@ -1,8 +1,17 @@
 package com.mashup.zuzu
 
 import android.app.Application
+import com.example.zuzu_android.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class ProofApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
