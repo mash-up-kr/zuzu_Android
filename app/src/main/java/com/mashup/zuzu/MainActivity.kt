bdbdf920db3a66101433dc5306146f690d3b89b7
Hyunkuk <boris0815@naver.com>
2022-07-23 17:24:22 +0900

    [refactor] 기타 수정
    - 월드컵 연결로 인한, 월드컵 버튼 람다 application 수준으로 이동
    - category viewModel 수정
    - manifest 수정

--- app/src/main/java/com/mashup/zuzu/MainActivity.kt
+++ app/src/main/java/com/mashup/zuzu/MainActivity.kt
@@ -27,7 +27,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = ProofTheme.color.black
                 ) {
-                    ZuzuApp()
+                    ZuzuApp(
+                        onWorldCupButtonClick = {}
+                    )
                 }
             }
         }
