46fcfdb3ab05e124396fd158ac68b5f68e536cde
Hyunkuk <boris0815@naver.com>
2022-07-26 18:20:04 +0900

    [refactor] 더미 레포지토리 삭제

--- app/src/main/java/com/mashup/zuzu/data/model/UserModel.kt
+++ app/src/main/java/com/mashup/zuzu/data/model/UserModel.kt
@@ -1,8 +1,6 @@
 package com.mashup.zuzu.data.model
 
 import androidx.compose.runtime.Immutable
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 /**
  * @Created by 김현국 2022/07/15
@@ -22,11 +20,9 @@ val user = User(
     email = "boris0815@naver.com",
     image = "https://cdn.icon-icons.com/icons2/2468/PNG/512/user_user_thump_user_icon_user_profile_icon_149320.png"
 )
-object UserRepo {
-    fun getUserData(): Flow<User> = flow {
-        emit(user)
-    }
-    fun getUser(id: Long) = user
-
-    fun submitUserProfile(username: String) {}
-}
+val dummyUser = User(
+    id = 0L,
+    name = "",
+    email = "",
+    image = ""
+)
