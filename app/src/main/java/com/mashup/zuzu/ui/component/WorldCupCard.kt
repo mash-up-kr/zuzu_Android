ad1e662c837e79326782b5a869953632197274aa
Hyunkuk <boris0815@naver.com>
2022-07-26 18:26:41 +0900

    [refactor] ImageRequest -> url로 변경

--- app/src/main/java/com/mashup/zuzu/ui/component/WorldCupCard.kt
+++ app/src/main/java/com/mashup/zuzu/ui/component/WorldCupCard.kt
@@ -11,13 +11,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.mashup.zuzu.R
 import com.mashup.zuzu.data.model.BestWorldCup
 import com.mashup.zuzu.data.model.bestWorldCupList
@@ -44,9 +42,7 @@ fun WorldCupCard(
             // Proof 태그 추가예정
             AsyncImage(
                 modifier = Modifier.height(88.dp).width(88.dp).clip(shape = CircleShape),
-                model = ImageRequest.Builder(LocalContext.current).data(
-                    worldCupItem.image
-                ).build(),
+                model = worldCupItem.image,
                 contentDescription = null,
                 contentScale = ContentScale.FillBounds
             )
