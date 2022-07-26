09e63bdd28577781c9b8e5d2d39f488a0e188500
Hyunkuk <boris0815@naver.com>
2022-07-26 18:19:14 +0900

    [feature] 유즈케이스 모듈 추가

---
+++ app/src/main/java/com/mashup/zuzu/data/di/UseCaseModule.kt
@@ -0,0 +1,80 @@
+package com.mashup.zuzu.data.di
+
+import com.mashup.zuzu.data.repository.CategoryRepository
+import com.mashup.zuzu.data.repository.HomeRepository
+import com.mashup.zuzu.data.repository.UserRepository
+import com.mashup.zuzu.domain.usecase.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * @Created by 김현국 2022/07/24
+ * @Time 5:13 오후
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+class UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideGetBestWorldCupListUseCase(homeRepository: HomeRepository): GetBestWorldCupListUseCase {
+        return GetBestWorldCupListUseCase(repository = homeRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetMainWineListUseCase(homeRepository: HomeRepository): GetMainWineListUseCase {
+        return GetMainWineListUseCase(repository = homeRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetRecommendWineUseCase(homeRepository: HomeRepository): GetRecommendWineUseCase {
+        return GetRecommendWineUseCase(repository = homeRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideJoinedWorldCupListUseCase(userRepository: UserRepository): GetJoinedWorldCupListUseCase {
+        return GetJoinedWorldCupListUseCase(repository = userRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetUserDataUseCase(userRepository: UserRepository): GetUserDataUseCase {
+        return GetUserDataUseCase(repository = userRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetWineCallerListUseCase(userRepository: UserRepository): GetWineCallerListUseCase {
+        return GetWineCallerListUseCase(repository = userRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUpdateUserProfileUseCase(userRepository: UserRepository): UpdateUserProfileUseCase {
+        return UpdateUserProfileUseCase(repository = userRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetWineListWithCategoryUseCase(categoryRepository: CategoryRepository): GetWineListWithCategoryUseCase {
+        return GetWineListWithCategoryUseCase(repository = categoryRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetWineListUseCase(categoryRepository: CategoryRepository): GetWineListUseCase {
+        return GetWineListUseCase(repository = categoryRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLeaveMembershipUseCase(userRepository: UserRepository): LeaveMembershipUseCase {
+        return LeaveMembershipUseCase(repository = userRepository)
+    }
+}
