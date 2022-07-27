c618bc941ba0319ce649d170150d98aaae6edd3a
Hyunkuk <boris0815@naver.com>
2022-07-27 17:51:26 +0900

    [refactor] 카테고리 아이템 ui 수정

--- app/src/main/java/com/mashup/zuzu/ui/component/CategoryItems.kt
+++ app/src/main/java/com/mashup/zuzu/ui/component/CategoryItems.kt
@@ -6,12 +6,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.mashup.zuzu.ui.model.Category
-import com.mashup.zuzu.ui.model.categoryList
+import com.mashup.zuzu.data.model.Category
+import com.mashup.zuzu.data.model.categoryList
 import com.mashup.zuzu.ui.theme.ProofTheme
 
 /**
@@ -19,21 +19,20 @@ import com.mashup.zuzu.ui.theme.ProofTheme
  * @Time 6:05 오후
  */
 
-
-
 @Composable
 fun CategoryItems(
     modifier: Modifier,
+    categoryList: List<Category>,
     onCategoryClick: (Category) -> Unit
 ) {
     LazyRow(
         modifier = modifier,
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         items(categoryList) { category ->
             CategoryCard(
                 modifier = Modifier.width(52.dp).height(52.dp)
-                    .background(color = ProofTheme.color.gray500, shape = RoundedCornerShape(16.dp)),
+                    .background(color = ProofTheme.color.gray500, shape = CircleShape),
                 category = category, onCategoryClick = onCategoryClick
             )
         }
