4e0717d57abc2ff2258b81f8cf367ca1b61856f7
Hyunkuk <boris0815@naver.com>
2022-08-01 16:05:27 +0900

    [refactor] home uiEvent파일 생성, 클릭이벤트 처리 , uiState 별도 파일 생성
    클릭 이벤트를 sealed class 로 묶어 처리하도록 했습니다

--- app/src/main/java/com/mashup/zuzu/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mashup/zuzu/ui/home/HomeViewModel.kt
@@ -25,32 +25,9 @@ import javax.inject.Inject
 
 /**
  * @Created by 김현국 2022/07/11
- * @Time 3:22 오후
  */
 
-sealed class BestWorldCupUiState {
-    object Loading : BestWorldCupUiState()
-    object Error : BestWorldCupUiState()
-    data class Success(val bestWorldCupList: List<BestWorldCup>) : BestWorldCupUiState()
-}
 
-sealed class RecommendWineUiState {
-    object Loading : RecommendWineUiState()
-    object Error : RecommendWineUiState()
-    data class Success(val recommendWine: Wine) : RecommendWineUiState()
-}
-
-sealed class MainWineUiState {
-    object Loading : MainWineUiState()
-    object Error : MainWineUiState()
-    data class Success(val mainWines: List<Wine>) : MainWineUiState()
-}
-
-sealed class CategoryListUiState {
-    object Loading : CategoryListUiState()
-    object Error : CategoryListUiState()
-    data class Success(val categoryList: List<Category>) : CategoryListUiState()
-}
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
