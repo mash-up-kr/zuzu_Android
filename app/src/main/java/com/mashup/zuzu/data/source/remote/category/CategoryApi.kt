ceb4bc0365a300d11601cbfa9477835c1ecb84cc
Hyunkuk <boris0815@naver.com>
2022-08-14 16:20:11 +0900

    [refactor] categoryApi, category Datasource 수정

--- app/src/main/java/com/mashup/zuzu/data/source/remote/category/CategoryApi.kt
+++ app/src/main/java/com/mashup/zuzu/data/source/remote/category/CategoryApi.kt
@@ -1,10 +1,8 @@
 package com.mashup.zuzu.data.source.remote.category
 
 import com.mashup.zuzu.data.response.GetDrinksCategoryResponse
-import com.mashup.zuzu.data.response.GetDrinksWithCategoryResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
 
 /**
  * @Created by 김현국 2022/08/12
@@ -13,10 +11,4 @@ interface CategoryApi {
 
     @GET("/drinks-category")
     suspend fun getDrinksCategory(): Response<List<GetDrinksCategoryResponse>>
-
-    // 특정 카테고리에 해당하는 주류 가져오기
-    @GET("/drinks/category")
-    suspend fun getDrinksWithCategory(
-        @Query("name") name: String
-    ): Response<List<GetDrinksWithCategoryResponse>>
 }
--- app/src/main/java/com/mashup/zuzu/data/source/remote/category/CategoryRemoteDataSource.kt
+++ app/src/main/java/com/mashup/zuzu/data/source/remote/category/CategoryRemoteDataSource.kt
@@ -1,21 +1,19 @@
 package com.mashup.zuzu.data.source.remote.category
 
-import com.mashup.zuzu.data.model.Category
-import com.mashup.zuzu.data.model.Results
-import com.mashup.zuzu.data.model.Wine
-import kotlinx.coroutines.flow.Flow
+import com.mashup.zuzu.data.response.GetDrinksCategoryResponse
+import retrofit2.Response
+import javax.inject.Inject
 
 /**
  * @Created by 김현국 2022/08/08
  */
-interface CategoryRemoteDataSource {
+
+class CategoryRemoteDataSource @Inject constructor(
+    private val categoryApi: CategoryApi
+) {
 
     // 주류 카테고리 가져오기
-    fun getDrinksCategory(): Flow<Results<List<Category>>>
-
-    // 특정 카테고리에 해당하는 주류 가져오기
-    fun getDrinksWithCategory(
-        category: String,
-        page: Int
-    ): Flow<Results<List<Wine>>>
+    suspend fun getDrinksCategory(): Response<List<GetDrinksCategoryResponse>> {
+        return categoryApi.getDrinksCategory()
+    }
 }
