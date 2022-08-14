748cd94908da8e88de64ca7befccac03235d3868
Hyunkuk <boris0815@naver.com>
2022-08-14 16:20:42 +0900

    [refactor] wineApi, wine Datasource 수정

--- app/src/main/java/com/mashup/zuzu/data/source/remote/wine/WineApi.kt
+++ app/src/main/java/com/mashup/zuzu/data/source/remote/wine/WineApi.kt
@@ -1,10 +1,12 @@
 package com.mashup.zuzu.data.source.remote.wine
 
 import com.mashup.zuzu.data.response.GetDrinksResponse
+import com.mashup.zuzu.data.response.GetDrinksWithCategoryResponse
 import com.mashup.zuzu.data.response.GetDrinksWithIdResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 /**
  * @Created by 김현국 2022/08/08
@@ -19,4 +21,16 @@ interface WineApi {
     @GET("/drinks/{id}")
     suspend fun getDrinksWithId(@Path("id") id: Int):
         Response<GetDrinksWithIdResponse>
+
+    // 특정 카테고리에 해당하는 주류 가져오기
+    @GET("/drinks/category")
+    suspend fun getDrinksWithCategory(
+        @Query("name") name: String,
+        @Query("page") page: Int,
+        @Query("length") length: Int
+    ): Response<GetDrinksWithCategoryResponse>
+
+    // 유저페이지 술 저장고 내부 술 리스트 가져오기
+    @GET("/drinks/reviews/users")
+    suspend fun getDrinksReviewsInUserPage(): Response<List<GetDrinksResponse>>
 }
--- app/src/main/java/com/mashup/zuzu/data/source/remote/wine/WineRemoteDataSource.kt
+++ app/src/main/java/com/mashup/zuzu/data/source/remote/wine/WineRemoteDataSource.kt
@@ -1,22 +1,45 @@
 package com.mashup.zuzu.data.source.remote.wine
 
-import com.mashup.zuzu.data.model.BestWorldCup
-import com.mashup.zuzu.data.model.Results
-import com.mashup.zuzu.data.model.Wine
-import kotlinx.coroutines.flow.Flow
+import com.mashup.zuzu.data.response.GetDrinksResponse
+import com.mashup.zuzu.data.response.GetDrinksWithCategoryResponse
+import com.mashup.zuzu.data.response.GetDrinksWithIdResponse
+import retrofit2.Response
+import timber.log.Timber
+import javax.inject.Inject
 
 /**
  * @Created by 김현국 2022/08/12
  */
-interface WineRemoteDataSource {
+class WineRemoteDataSource @Inject constructor(
+    private val wineApi: WineApi
+) {
 
-    fun getDrinks(): Flow<Results<List<Wine>>>
+    suspend fun getDrinks(): Response<List<GetDrinksResponse>> {
+        return wineApi.getDrinks()
+    }
 
-    fun getDrinksWithId(
+    suspend fun getDrinksWithId(
         id: Int
-    ): Flow<Results<Wine>>
+    ): Response<GetDrinksWithIdResponse> {
+        return wineApi.getDrinksWithId(id = id)
+    }
 
-    fun getRecommendWine(): Flow<Results<Wine>>
+    // 특정 카테고리에 해당하는 주류 가져오기
+    suspend fun getDrinksWithCategory(
+        name: String,
+        page: Int,
+        length: Int
+    ): Response<GetDrinksWithCategoryResponse> {
+        Timber.tag("threadDataSource").d(Thread.currentThread().toString())
+        return wineApi.getDrinksWithCategory(
+            name = name,
+            page = page,
+            length = length
+        )
+    }
 
-    fun getBestWorldCupList(): Flow<Results<List<BestWorldCup>>>
+    // 유저페이지 술 저장고 내부 술 리스트 가져오기
+    suspend fun getDrinksReviewsInUserPage(): Response<List<GetDrinksResponse>> {
+        return wineApi.getDrinksReviewsInUserPage()
+    }
 }
