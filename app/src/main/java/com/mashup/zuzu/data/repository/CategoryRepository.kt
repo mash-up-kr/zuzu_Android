3195fd8af6762d0569835151ccf039e0c030d1b7
Hyunkuk <boris0815@naver.com>
2022-08-14 16:18:47 +0900

    [refactor] worldCup repository 생성  및 User & Category repository 수정

--- app/src/main/java/com/mashup/zuzu/data/repository/CategoryRepository.kt
+++ app/src/main/java/com/mashup/zuzu/data/repository/CategoryRepository.kt
@@ -2,8 +2,8 @@ package com.mashup.zuzu.data.repository
 
 import com.mashup.zuzu.data.mapper.categoryResponseToModel
 import com.mashup.zuzu.data.model.*
-import com.mashup.zuzu.data.source.remote.category.CategoryApi
 import com.mashup.zuzu.data.source.remote.category.CategoryRemoteDataSource
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -14,28 +14,18 @@ import javax.inject.Inject
  * @Created by 김현국 2022/07/24
  */
 class CategoryRepository @Inject constructor(
-    private val categoryApi: CategoryApi
-) : CategoryRemoteDataSource {
-    override fun getDrinksCategory(): Flow<Results<List<Category>>> {
+    private val categoryRemoteDataSource: CategoryRemoteDataSource,
+    private val ioDispatcher: CoroutineDispatcher
+) {
+    fun getDrinksCategory(): Flow<Results<List<Category>>> {
         return flow {
             emit(Results.Loading)
-            val response = categoryApi.getDrinksCategory()
+            val response = categoryRemoteDataSource.getDrinksCategory()
             val body = response.body()
             if (response.isSuccessful && body != null) {
                 val data = categoryResponseToModel(categoryResponse = body)
                 emit(Results.Success(data))
             }
-        }.flowOn(Dispatchers.IO)
-    }
-
-    override fun getDrinksWithCategory(category: String, page: Int): Flow<Results<List<Wine>>> {
-        return flow {
-            emit(Results.Loading)
-            emit(
-                Results.Success(
-                    PageWineRepo.getWineListWithPage(pageNumber = page, category = category).wines
-                )
-            )
-        }.flowOn(Dispatchers.IO)
+        }.flowOn(ioDispatcher)
     }
 }
--- app/src/main/java/com/mashup/zuzu/data/repository/UserRepository.kt
+++ app/src/main/java/com/mashup/zuzu/data/repository/UserRepository.kt
@@ -1,9 +1,9 @@
 package com.mashup.zuzu.data.repository
 
+import com.mashup.zuzu.data.mapper.userProfileImagesResponseToModel
 import com.mashup.zuzu.data.model.*
-import com.mashup.zuzu.data.source.remote.user.UserApi
 import com.mashup.zuzu.data.source.remote.user.UserRemoteDataSource
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -14,45 +14,44 @@ import javax.inject.Inject
  * @Created by 김현국 2022/07/24
  */
 class UserRepository @Inject constructor(
-    private val userApi: UserApi
-) : UserRemoteDataSource {
-    override fun getUserData(userId: Long): Flow<Results<User>> {
+    private val userRemoteDataSource: UserRemoteDataSource,
+    private val ioDispatcher: CoroutineDispatcher
+) {
+    fun getUserData(userId: Long): Flow<Results<User>> {
         return flow {
             emit(Results.Loading)
             delay(1000)
             emit(Results.Success(user))
-        }.flowOn(Dispatchers.IO)
+        }.flowOn(ioDispatcher)
     }
 
-    override fun getWineCallerList(userId: Long): Flow<Results<List<Wine>>> {
-        return flow {
-            emit(Results.Loading)
-            delay(1000)
-            emit(Results.Success(wines))
-        }.flowOn(Dispatchers.IO)
-    }
-
-    override fun getJoinedWorldCupList(userId: Long): Flow<Results<List<BestWorldCup>>> {
-        return flow {
-            emit(Results.Loading)
-            delay(1000)
-            emit(Results.Success(bestWorldCupList))
-        }.flowOn(Dispatchers.IO)
-    }
-
-    override fun updateUserProfile(): Flow<Results<String>> {
+    fun updateUserProfile(): Flow<Results<String>> {
         return flow {
             emit(Results.Loading)
             delay(1000)
             emit(Results.Success("완료했습니다"))
-        }.flowOn(Dispatchers.IO)
+        }.flowOn(ioDispatcher)
     }
 
-    override fun leaveMembership(userId: Long): Flow<Results<String>> {
+    fun getUserProfileImages(): Flow<Results<List<UserProfileImagesModel>>> {
         return flow {
             emit(Results.Loading)
-            delay(1000)
-            emit(Results.Success("탈퇴 성공"))
-        }.flowOn(Dispatchers.IO)
+            val response = userRemoteDataSource.getUserProfileImages()
+            val body = response.body()
+            if (response.isSuccessful && body != null) {
+                val data = userProfileImagesResponseToModel(body)
+                emit(Results.Success(data))
+            }
+        }
+    }
+
+    fun deleteUser(): Flow<Results<String>> {
+        return flow {
+            emit(Results.Loading)
+            val response = userRemoteDataSource.deleteUser()
+            if (response.isSuccessful) {
+                emit(Results.Success("탍퇴 성공"))
+            }
+        }.flowOn(ioDispatcher)
     }
 }
