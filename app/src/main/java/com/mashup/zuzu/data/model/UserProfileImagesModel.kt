d1c746234dfc46597e08a5093e5d725a28ff5f7e
Hyunkuk <boris0815@naver.com>
2022-08-16 16:36:55 +0900

    [refactor] 모델 이름 수정

--- app/src/main/java/com/mashup/zuzu/data/model/UserProfileImagesModel.kt
+++ app/src/main/java/com/mashup/zuzu/data/model/UserProfileImagesModel.kt
@@ -4,7 +4,7 @@ package com.mashup.zuzu.data.model
  * @Created by 김현국 2022/08/13
  */
 
-data class UserProfileImagesModel(
+data class UserProfileImages(
     val id: Long,
     val image_url: String
 )
