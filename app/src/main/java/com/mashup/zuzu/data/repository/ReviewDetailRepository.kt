f962a7dd6609107aeb83601450d284821e1056b5
Hyunkuk <boris0815@naver.com>
2022-08-20 16:00:39 +0900

    [refactor] remove price, favorite

--- app/src/main/java/com/mashup/zuzu/data/repository/ReviewDetailRepository.kt
+++ app/src/main/java/com/mashup/zuzu/data/repository/ReviewDetailRepository.kt
@@ -1,7 +1,6 @@
 package com.mashup.zuzu.data.repository
 
 import com.mashup.zuzu.data.model.Wine
-import com.mashup.zuzu.ui.review.ReviewDetailUiState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -13,12 +12,10 @@ class ReviewDetailRepository {
                     id = 1L,
                     name = "GoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlue",
                     imageUrl = "https://images.absolutdrinks.com/ingredient-images/Raw/Absolut/65d43459-c926-4b12-a15b-afa7a71c2071.jpg?imwidth=500",
-                    price = 1000,
                     alc = 17,
                     description = "뜨는 술",
                     category = "와인",
-                    tags = listOf("뜨는 술", "맛있는 술"),
-                    favorite = true
+                    tags = listOf("뜨는 술", "맛있는 술")
                 )
             )
         }
--- app/src/main/java/com/mashup/zuzu/data/repository/ReviewWriteRepository.kt
+++ app/src/main/java/com/mashup/zuzu/data/repository/ReviewWriteRepository.kt
@@ -5,16 +5,14 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class ReviewWriteRepository constructor() {
-    //TODO: Wine Repository를 따로 분리해야할 수도?
+    // TODO: Wine Repository를 따로 분리해야할 수도?
     fun getReviewWineStream(): Flow<Wine> = flow {
         Wine(
             id = 1L,
             name = "GoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlueGoldenBlue",
             imageUrl = "https://images.absolutdrinks.com/ingredient-images/Raw/Absolut/65d43459-c926-4b12-a15b-afa7a71c2071.jpg?imwidth=500",
-            price = 1000,
             alc = 17,
             description = "하이하이하이하이",
-            favorite = true,
             category = "와인",
             tags = listOf("뜨는 술", "맛있는 술", "쓴 술", "단 술")
         )
