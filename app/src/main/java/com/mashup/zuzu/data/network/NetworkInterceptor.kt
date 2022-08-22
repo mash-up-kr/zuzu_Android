cca3542947175ab55795d8d44491368bd00ffbf2
Hyunkuk <boris0815@naver.com>
2022-08-22 18:16:48 +0900

    [refactor] accessToken NetWork에 추가

--- app/src/main/java/com/mashup/zuzu/data/network/NetworkInterceptor.kt
+++ app/src/main/java/com/mashup/zuzu/data/network/NetworkInterceptor.kt
@@ -1,17 +1,25 @@
 package com.mashup.zuzu.data.network
 
+import com.mashup.zuzu.bridge.ProofPreference
 import okhttp3.Interceptor
 import okhttp3.Response
+import timber.log.Timber
+import javax.inject.Inject
 
 /**
  * @Created by 김현국 2022/08/08
  */
-class NetworkInterceptor : Interceptor {
+class NetworkInterceptor @Inject constructor(
+    private val proofPreference: ProofPreference
+) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = proofPreference.get("accessToken", "")
+
         return chain.proceed(
             request = chain.request().newBuilder().apply {
-                header("accept", "*/*") // token
+                header("accept" , "application/json")
+                header("Authorization", "Bearer $accessToken") // token
             }
                 .build()
         )
