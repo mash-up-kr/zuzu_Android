66c2804679281085aacf4284ad3edc4c4bd7f68a
Hyunkuk <boris0815@naver.com>
2022-08-22 18:17:55 +0900

    [refactor] userWorldCup response 수정 및 연결

--- app/src/main/java/com/mashup/zuzu/data/repository/WorldCupRepository.kt
+++ app/src/main/java/com/mashup/zuzu/data/repository/WorldCupRepository.kt
@@ -1,16 +1,13 @@
 package com.mashup.zuzu.data.repository
 
-import com.mashup.zuzu.ui.model.BestWorldCup
 import com.mashup.zuzu.data.model.Results
-import com.mashup.zuzu.ui.model.bestWorldCupList
-import com.mashup.zuzu.data.response.GetWorldCupPopularResponse
+import com.mashup.zuzu.data.response.GetWorldCupUserParticipatedResponse
+import com.mashup.zuzu.data.response.model.WorldCup
 import com.mashup.zuzu.data.source.remote.worldcup.WorldCupRemoteDataSource
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
-import javax.inject.Inject
 
 /**
  * @Created by 김현국 2022/08/13
@@ -20,7 +17,7 @@ class WorldCupRepository constructor(
     private val ioDispatcher: CoroutineDispatcher
 ) {
 
-    fun getBestWorldCupList(): Flow<Results<List<GetWorldCupPopularResponse>>> {
+    fun getBestWorldCupList(): Flow<Results<List<WorldCup>>> {
         return flow {
             emit(Results.Loading)
             val response = worldCupRemoteDataSource.getPopularWorldCup()
@@ -32,11 +29,16 @@ class WorldCupRepository constructor(
         }.flowOn(ioDispatcher)
     }
 
-    fun getJoinedWorldCupList(userId: Long): Flow<Results<List<BestWorldCup>>> {
+    fun getJoinedWorldCupList(): Flow<Results<List<GetWorldCupUserParticipatedResponse>>> {
         return flow {
             emit(Results.Loading)
-            delay(1000)
-            emit(Results.Success(bestWorldCupList))
+            val response = worldCupRemoteDataSource.getWorldCupUserParticipated()
+            val body = response.body()
+            if (response.isSuccessful && body != null) {
+                emit(Results.Success(body))
+            } else {
+                emit(Results.Success(emptyList()))
+            }
         }.flowOn(ioDispatcher)
     }
 }
--- app/src/main/java/com/mashup/zuzu/domain/usecase/GetBestWorldCupListUseCase.kt
+++ app/src/main/java/com/mashup/zuzu/domain/usecase/GetBestWorldCupListUseCase.kt
@@ -2,7 +2,7 @@ package com.mashup.zuzu.domain.usecase
 
 import com.mashup.zuzu.data.model.Results
 import com.mashup.zuzu.data.repository.WorldCupRepository
-import com.mashup.zuzu.data.response.GetWorldCupPopularResponse
+import com.mashup.zuzu.data.response.model.WorldCup
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -15,7 +15,7 @@ import javax.inject.Singleton
 class GetBestWorldCupListUseCase @Inject constructor(
     private val repository: WorldCupRepository
 ) {
-    operator fun invoke(): Flow<Results<List<GetWorldCupPopularResponse>>> {
+    operator fun invoke(): Flow<Results<List<WorldCup>>> {
         return repository.getBestWorldCupList()
     }
 }
