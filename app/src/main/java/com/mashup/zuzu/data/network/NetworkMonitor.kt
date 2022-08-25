869b3145f6bbab99bab60f0d986706ade31b08f0
Hyunkuk <boris0815@naver.com>
2022-08-25 19:51:50 +0900

    [feature] ConnectivityManager로 인터넷 연결상태를 캐치

---
+++ app/src/main/java/com/mashup/zuzu/data/network/NetworkMonitor.kt
@@ -0,0 +1,49 @@
+package com.mashup.zuzu.data.network
+
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+/**
+ * @Created by 김현국 2022/08/25
+ */
+class NetworkMonitor @Inject constructor(
+    private val connectivityManager: ConnectivityManager
+) {
+
+    val isConnected: Flow<Boolean> = callbackFlow {
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                trySend(true)
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                trySend(false)
+            }
+        }
+        val request = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+                }
+            }
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .build()
+        trySend(ConnectedCompat.isConnected(connectivityManager))
+        connectivityManager.registerNetworkCallback(request, callback)
+
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(callback)
+        }
+    }
+}
