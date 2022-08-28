6dff0cdc0236f7f2e8746a26fea9d6442aa18621
d2fault <d2fault@naver.com>
2022-08-28 17:32:49 +0900

    [feature] 카카오 계정 로그인 기능 추가
    - 카카오톡 미설치 케이스 지원을 위해 계정 로그인 기능을 추가합니다.

--- app/src/main/java/com/mashup/zuzu/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mashup/zuzu/ui/login/LoginActivity.kt
@@ -11,6 +11,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import com.mashup.zuzu.MainActivity
 import com.mashup.zuzu.R
@@ -59,7 +62,7 @@ class LoginActivity : AppCompatActivity() {
                 viewModel.actionFlow.collect { action ->
                     if (canUseNetwork) {
                         when (action) {
-                            ClickKakaoLogin -> getKakaoAccessToken()
+                            ClickKakaoLogin -> Kakao().getKakaoAccessToken()
                             ProofAuthFailed ->
                                 Toast.makeText(
                                     this@LoginActivity,
@@ -113,14 +116,46 @@ class LoginActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun getKakaoAccessToken() {
-        UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
-            if (error != null) {
-                Timber.e("getKakaoAccessToken: Failed / error: ", error)
-            } else if (token != null) {
-                Timber.d("getKakaoAccessToken: Success / accessToken: ${token.accessToken}")
-                viewModel.getProofAuthData(token.accessToken)
+    private inner class Kakao {
+        fun getKakaoAccessToken() {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@LoginActivity)) {
+                loginWithKakaoTalk()
+            } else {
+                loginWithKakaoAccount()
             }
+        }
+
+        private fun loginWithKakaoTalk() {
+            UserApiClient.instance.loginWithKakaoTalk(this@LoginActivity) { token, error ->
+                if (error != null) {
+                    Timber.e("getKakaoAccessToken: Failed / error: ", error)
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    loginWithKakaoAccount()
+                } else if (token != null) {
+                    Timber.d("getKakaoAccessToken: Success / accessToken: ${token.accessToken}")
+                    viewModel.getProofAuthData(token.accessToken)
+                }
+            }
+        }
+
+        private fun loginWithKakaoAccount() {
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Timber.e("getKakaoAccessToken: Failed / error: ", error)
+                } else if (token != null) {
+                    Timber.d("getKakaoAccessToken: Success / accessToken: ${token.accessToken}")
+                    viewModel.getProofAuthData(token.accessToken)
+                }
+            }
+            UserApiClient.instance.loginWithKakaoAccount(
+                this@LoginActivity,
+                callback = callback
+            )
         }
     }
 }
