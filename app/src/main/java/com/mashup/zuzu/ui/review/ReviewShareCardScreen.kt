0722562e86aa28d1cc7e7f7d9cda94129e573b15
Teddy <aackddlr@gmail.com>
2022-09-01 00:20:08 +0900

    [fix] 리뷰 결과 화면 공유 카드 블러 이미지 변경 적용

--- app/src/main/java/com/mashup/zuzu/ui/review/ReviewShareCardScreen.kt
+++ app/src/main/java/com/mashup/zuzu/ui/review/ReviewShareCardScreen.kt
@@ -1,6 +1,7 @@
 package com.mashup.zuzu.ui.review
 
 import android.graphics.Bitmap
+import android.os.Build
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -16,7 +17,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.mashup.zuzu.compose.component.ShareReviewCard
+import com.mashup.zuzu.compose.component.ShareReviewCardWithRenderScript
 import com.mashup.zuzu.compose.theme.ProofTheme
+import com.mashup.zuzu.data.mapper.reviewShareCardToListModel
 import com.mashup.zuzu.data.model.ReviewShareCard
 import com.mashup.zuzu.util.saveBitmapToStorage
 import dev.shreyaspatil.capturable.Capturable
@@ -135,180 +138,18 @@ fun ReviewCard(
             }
         }
     ) {
-//    Box(
-//        modifier = modifier
-//            .fillMaxWidth()
-//            .fillMaxHeight()
-//    ) {
-//        Box(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .height(396.dp)
-//                .background(
-//                    brush = Brush.verticalGradient(
-//                        colors = listOf(
-//                            ProofTheme.color.gray500,
-//                            ProofTheme.color.gray500
-//                        )
-//                    ),
-//                    alpha = 0.6f,
-//                    shape = RoundedCornerShape(12.dp)
-//                )
-//                .align(Alignment.BottomCenter)
-//        )
-//
-//        Box(
-//            modifier = Modifier
-//                .padding(horizontal = 16.dp)
-//                .align(Alignment.TopCenter)
-//        ) {
-//            Card(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(330.dp),
-//                shape = RoundedCornerShape(12.dp)
-//            ) {
-//                BlurWithOuterHeightImage(blurOuterHeight = 740.dp.value) {
-//                    AsyncImage(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .fillMaxHeight(),
-//                        model = ImageRequest.Builder(LocalContext.current)
-//                            .data(reviewShareCard.wine.imageUrl).build(),
-//                        contentDescription = null,
-//                        contentScale = ContentScale.FillHeight
-//                    )
-//                }
-//            }
-//
-//            Image(
-//                painter = painterResource(id = R.drawable.ic_share_logo), contentDescription = "",
-//                modifier = Modifier
-//                    .padding(12.dp)
-//                    .size(70.dp, 24.dp)
-//            )
-//        }
-//
-//        Column(
-//            modifier = Modifier
-//                .align(Alignment.BottomCenter)
-//        ) {
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(end = 30.dp),
-//                horizontalAlignment = Alignment.End
-//            ) {
-//                val categoryColor = getDrinkCategoryColor(reviewShareCard.wine.category)
-//
-//                Text(
-//                    text = "NO.002",
-//                    style = ProofTheme.typography.headingXS,
-//                    color = ProofTheme.color.gray500,
-//                    textAlign = TextAlign.Center,
-//                    modifier = Modifier
-//                        .padding(bottom = 14.dp)
-//                        .background(
-//                            color = Color(categoryColor),
-//                            shape = RoundedCornerShape(4.dp)
-//                        )
-//                        .padding(6.dp)
-//                )
-//
-//                Text(
-//                    text = reviewShareCard.userReview.createdAt.split("T")[0],
-//                    style = ProofTheme.typography.headingXS,
-//                    color = Color(categoryColor),
-//                    textAlign = TextAlign.Center,
-//                    modifier = Modifier
-//                        .padding(bottom = 16.dp)
-//                        .background(
-//                            color = ProofTheme.color.gray500,
-//                            shape = RoundedCornerShape(4.dp)
-//                        )
-//                        .padding(6.dp)
-//                )
-//            }
-//
-//            Column(
-//                modifier = Modifier
-//                    .background(
-//                        brush = Brush.verticalGradient(
-//                            colors = listOf(
-//                                ProofTheme.color.gray500,
-//                                ProofTheme.color.gray500
-//                            )
-//                        ),
-//                        alpha = 0.6f,
-//                        shape = RoundedCornerShape(bottomStart = 12.dp, bottomEnd = 12.dp)
-//                    ),
-// //                    .align(Alignment.BottomCenter),
-//                horizontalAlignment = Alignment.CenterHorizontally
-//            ) {
-//                /**
-//                 * Drink Description
-//                 */
-//                Text(
-//                    text = reviewShareCard.wine.name,
-//                    color = ProofTheme.color.white,
-//                    style = ProofTheme.typography.headingS,
-//                    maxLines = 2,
-//                    modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 12.dp)
-//                )
-//
-//                Spacer(
-//                    modifier = Modifier
-//                        .padding(vertical = 12.dp)
-//                        .fillMaxWidth()
-//                        .height(1.dp)
-//                        .background(color = ProofTheme.color.black)
-//                )
-//
-//                /**
-//                 * Drink Info
-//                 */
-//                Text(
-//                    text = reviewShareCard.wine.category,
-//                    color = ProofTheme.color.white,
-//                    style = ProofTheme.typography.headingXS,
-//                    modifier = Modifier.padding(horizontal = 8.dp)
-//                )
-//
-//                Text(
-//                    text = "ALC ${reviewShareCard.wine.alc}% . FROM reviewShareCard.wine.origin",
-//                    color = ProofTheme.color.white,
-//                    style = ProofTheme.typography.buttonM,
-//                    textAlign = TextAlign.Center,
-//                    modifier = Modifier.padding(horizontal = 8.dp)
-//                )
-//
-//                Spacer(
-//                    modifier = Modifier
-//                        .padding(vertical = 12.dp)
-//                        .fillMaxWidth()
-//                        .height(1.dp)
-//                        .background(color = ProofTheme.color.black)
-//                )
-//
-//                /**
-//                 * Review Info
-//                 */
-//                Text(
-//                    text = "${reviewShareCard.userReview.mood} ${reviewShareCard.userReview.weather},${reviewShareCard.userReview.time}" +
-//                            "\n${reviewShareCard.userReview.isHeavy}|${reviewShareCard.userReview.isBitter}|${reviewShareCard.userReview.isBurning}|${reviewShareCard.userReview.isStrong}",
-//                    color = ProofTheme.color.white,
-//                    style = ProofTheme.typography.bodyS,
-//                    textAlign = TextAlign.Center,
-//                    modifier = Modifier.padding(bottom = 12.dp)
-//                )
-//            }
-//        }
-//    }
-
-        ShareReviewCard(
-            reviewShareCard = reviewShareCard,
-            modifier = Modifier.padding(start = 24.dp, end = 24.dp, top = 16.dp)
-        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            ShareReviewCard(
+                reviewShareCard = reviewShareCard,
+                modifier = Modifier.padding(start = 24.dp, end = 24.dp, top = 16.dp, bottom = 10.dp)
+            )
+        } else {
+            ShareReviewCardWithRenderScript(
+                modifier = Modifier.padding(start = 24.dp, end = 24.dp, top = 16.dp, bottom = 10.dp),
+                reviewShareCard = reviewShareCard,
+                blurImage = reviewShareCard.wine.bitmap
+            )
+        }
     }
 }
 
