2d51b7e1c1c40d086ebfc470050116a2ebb06735
Hyunkuk <boris0815@naver.com>
2022-10-15 14:38:12 +0900

    fix : BottomNavigation

--- app/src/main/java/com/mashup/zuzu/ZuzuApp.kt
+++ app/src/main/java/com/mashup/zuzu/ZuzuApp.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -133,13 +132,5 @@ fun ZuzuApp(
                 }
             }
         }
-    }
-}
-
-@Preview
-@Composable
-fun PreviewZuzuApp() {
-    ProofTheme() {
-        ZuzuApp({})
     }
 }
